76545667dbebc9ce924d2b10395630d88cfc2a34
mars-86 <hrslash86@gmail.com>
2023-04-02 20:35:22 -0300

    initial commit

---
+++ drawminal/pixelmap.h
@@ -0,0 +1,11 @@
+#ifndef __DRAWMINAL_PIXEL_MAP_INCLUDED_H__
+#define __DRAWMINAL_PIXEL_MAP_INCLUDED_H__
+
+const unsigned int pixel_map[4][2] = {
+    {0x01, 0x08},
+    {0x02, 0x10},
+    {0x04, 0x20},
+    {0x40, 0x80}
+};
+
+#endif // __DRAWMINAL_PIXEL_MAP_INCLUDED_H__
